9c50cef31a4827e3d5fc9bf61a8c8f7fb1eddb1b
sharmasakshii <161819423+sharmasakshii@users.noreply.github.com>
2024-10-21 15:54:38 +0530

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -132,7 +132,52 @@ def newitem(req: func.HttpRequest) -> func.HttpResponse:
         )
     
 register_route("newitem", ["GET"])
+
+@app.route(route="checkitem", methods=["GET"])
+def checkitem(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
  
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+ 
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
+    
+register_route("checkitem", ["GET"])
+
+@app.route(route="slotitem", methods=["GET"])
+def slotitem(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+ 
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+ 
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
+    
+register_route("slotitem", ["GET"])
  
 
  

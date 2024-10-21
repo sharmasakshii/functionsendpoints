e546b2042f1dae88e4be04ebf23b906326efe99a
sharmasakshii <161819423+sharmasakshii@users.noreply.github.com>
2024-10-21 16:20:31 +0530

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -154,30 +154,7 @@ def checkitem(req: func.HttpRequest) -> func.HttpResponse:
              status_code=200
         )
     
-register_route("checkitem", ["GET"])
+register_route("checkitem", ["POST"])
 
-@app.route(route="slotitem", methods=["GET"])
-def slotitem(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
- 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
- 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
-    
-register_route("slotitem", ["GET"])
- 
 
  
